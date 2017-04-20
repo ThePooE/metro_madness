2501c2faa6a16af4d633033f2747b2584a3c12b9
Nate Bhurinat Wangsutthitham <thepooe@gmail.com>
2017-04-20 15:03:31 +1000

    Remove duplicate code

--- core/src/com/unimelb/swen30006/metromadness/stations/CargoStation.java
+++ core/src/com/unimelb/swen30006/metromadness/stations/CargoStation.java
@@ -6,6 +6,7 @@ import com.unimelb.swen30006.metromadness.passengers.Passenger;
 import com.unimelb.swen30006.metromadness.passengers.PassengerGenerator;
 import com.unimelb.swen30006.metromadness.routers.PassengerRouter;
 import com.unimelb.swen30006.metromadness.tracks.Line;
+import com.unimelb.swen30006.metromadness.trains.CargoTrain;
 import com.unimelb.swen30006.metromadness.trains.Train;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -18,7 +19,7 @@ import java.util.Iterator;
  * https://github.com/natebwangsut
  */
 
-public class CargoStation extends Station {
+public class CargoStation extends ActiveStation {
 
     private static Logger logger = LogManager.getLogger();
 
@@ -28,48 +29,15 @@ public class CargoStation extends Station {
 
 
     public CargoStation(float x, float y, PassengerRouter router, String name, float maxPax) {
-        super(x, y, router, name);
+        super(x, y, router, name, maxPax);
         this.waiting = new ArrayList<Passenger>();
         this.g = new PassengerGenerator(this, this.lines, maxPax);
         this.maxVolume = maxPax;
     }
 
     @Override
-    public void enter(Train t) throws Exception {
-        if(trains.size() >= PLATFORMS){
-            throw new Exception();
-        } else {
-            // Add the train
-            this.trains.add(t);
-            // Add the waiting passengers
-            Iterator<Passenger> pIter = this.waiting.iterator();
-            while(pIter.hasNext()){
-                Passenger p = pIter.next();
-                try {
-                    logger.info("Passenger "+p.id+" carrying "+p.getCargo().getWeight() +" kg cargo embarking at "+this.name+" heading to "+p.destination.name);
-                    t.embark(p);
-                    pIter.remove();
-                } catch (Exception e){
-                    // Do nothing, already waiting
-                    break;
-                }
-            }
-
-            // Do not add new passengers if there are too many already
-            if (this.waiting.size() > maxVolume){
-                return;
-            }
-            // Add the new passenger
-            Passenger[] ps = this.g.generatePassengers();
-            for(Passenger p: ps){
-                try {
-                    logger.info("Passenger "+p.id+" carrying "+p.getCargo().getWeight() +" kg embarking at "+this.name+" heading to "+p.destination.name);
-                    t.embark(p);
-                } catch(Exception e){
-                    this.waiting.add(p);
-                }
-            }
-        }
+    public boolean compatible(Train t) throws Exception {
+        return true;
     }
 
     public void render(ShapeRenderer renderer){
@@ -82,8 +50,6 @@ public class CargoStation extends Station {
             radius = radius - 1;
         }
 
-        renderer.rect(this.position.x, this.position.y, 5,5);
-
         // Calculate the percentage
         float t = this.trains.size()/(float)PLATFORMS;
         Color c = Color.WHITE.cpy().lerp(Color.DARK_GRAY, t);
@@ -92,7 +58,7 @@ public class CargoStation extends Station {
         }
 
         renderer.setColor(c);
-        renderer.rect(this.position.x, this.position.y, radius*2, radius*2);
+        renderer.rect(this.position.x+1, this.position.y+1, radius*2, radius*2);
         //renderer.circle(this.position.x, this.position.y, radius, NUM_CIRCLE_STATMENTS);
     }
 
