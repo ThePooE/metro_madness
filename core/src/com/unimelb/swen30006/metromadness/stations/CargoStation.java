9da813d989cdf381084d858ce6a04dc9ca113bd2
khaic <slchin_56@hotmail.com>
2017-04-23 22:36:59 +1000

    Fixed generator

--- core/src/com/unimelb/swen30006/metromadness/stations/CargoStation.java
+++ core/src/com/unimelb/swen30006/metromadness/stations/CargoStation.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.unimelb.swen30006.metromadness.exceptions.FullPlatformException;
 import com.unimelb.swen30006.metromadness.passengers.Passenger;
 import com.unimelb.swen30006.metromadness.passengers.PassengerGenerator;
 import com.unimelb.swen30006.metromadness.routers.PassengerRouter;
@@ -11,6 +12,10 @@ import com.unimelb.swen30006.metromadness.tracks.Line;
 import com.unimelb.swen30006.metromadness.trains.Train;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Nate Bhurinat W. (@natebwangsut | nate.bwangsut@gmail.com)
@@ -18,6 +23,9 @@ import java.util.ArrayList;
  */
 
 public class CargoStation extends ActiveStation {
+
+    // Logger
+    private static Logger logger = LogManager.getLogger();
 
 
     public PassengerGenerator g;
@@ -53,5 +61,49 @@ public class CargoStation extends ActiveStation {
 
         renderer.setColor(c);
         renderer.circle(this.position.x, this.position.y, radius, NUM_CIRCLE_STATMENTS);
+    }
+    
+    @Override
+    public void enter(Train t) throws Exception {
+        if(trains.size() >= PLATFORMS){
+            throw new FullPlatformException();
+        } else {
+            // Add the train
+            this.trains.add(t);
+            // Add the waiting passengers
+            Iterator<Passenger> pIter = this.waiting.iterator();
+            while(pIter.hasNext()){
+                Passenger p = pIter.next();
+                try {
+                    logger.info("Passenger "+p.id+" carrying "+p.getCargo().getWeight() +" kg cargo embarking at "+this.name+" heading to "+p.destination.name);
+                    t.embark(p);
+                    pIter.remove();
+                } catch (Exception e){
+                    // Do nothing, already waiting
+                    break;
+                }
+            }
+
+            // Do not add new passengers if there are too many already
+            if (this.waiting.size() > maxVolume){
+                return;
+            }
+
+            // Add the new passenger
+            Passenger[] ps = this.g.generatePassengers();
+            for(Passenger p: ps){
+                try {
+                    
+                    if(p==null){
+                        return;
+                    }
+                    
+                    logger.info("Passenger "+p.id+" carrying "+p.getCargo().getWeight() +" kg embarking at "+this.name+" heading to "+p.destination.name);
+                    t.embark(p);
+                } catch(Exception e){
+                    this.waiting.add(p);
+                }
+            }
+        }
     }
 }
