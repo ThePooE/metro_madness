7c864643f11af02b421124f88016d89e2920c17c
Nate Bhurinat Wangsutthitham <nate.bwangsut@gmail.com>
2017-04-27 21:56:07 +1000

    Remove duplicate codes

--- core/src/com/unimelb/swen30006/metromadness/stations/CargoStation.java
+++ core/src/com/unimelb/swen30006/metromadness/stations/CargoStation.java
@@ -64,21 +64,6 @@ public class CargoStation extends ActiveStation {
         renderer.circle(this.position.x, this.position.y, radius, NUM_CIRCLE_STATMENTS);
     }
 
-
-//    @Override
-//    public void renderWaiting(SpriteBatch b, BitmapFont header, boolean waiting){
-//        if(waiting){
-//            b.begin();
-//            header.getData().setScale(1f);
-//            header.draw(
-//                    b,
-//                    Integer.toString(this.waiting.size()),
-//                    this.position.x-10,
-//                    this.position.y-10);
-//            b.end();
-//        }
-//    }
-
     @Override
     // Generate passengers when a train has entered the station
     public void enter(Train t) throws Exception {
@@ -109,24 +94,4 @@ public class CargoStation extends ActiveStation {
             }
         }
     }
-
-//    /**
-//     * Embarking passengers onto train
-//     * @param t     Train for passengers to get on
-//     */
-//    public void addWaitingPassengers(Train t){
-//        Iterator<Passenger> pIter = this.waiting.iterator();
-//        while(pIter.hasNext()){
-//            Passenger p = pIter.next();
-//            try {
-//                logger.info("Passenger " + p.getID() + " carrying " + p.getCargo().getWeight()
-//                        + " kg cargo embarking at " + this.name + " heading to "+p.getDestination().name);
-//                t.embark(p);
-//                pIter.remove();
-//            } catch (Exception e){
-//                // Do nothing, already waiting
-//                break;
-//            }
-//        }
-//    }
 }
