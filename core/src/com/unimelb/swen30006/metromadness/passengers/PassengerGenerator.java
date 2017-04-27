e6eb83e2f93ea2b58a5e1fbd057413a98d8001bb
khaic <slchin_56@hotmail.com>
2017-04-27 21:05:44 +1000

    Authorship

--- core/src/com/unimelb/swen30006/metromadness/passengers/PassengerGenerator.java
+++ core/src/com/unimelb/swen30006/metromadness/passengers/PassengerGenerator.java
@@ -1,3 +1,16 @@
+/**
+ * SWEN30006 Software Modelling and Design
+ * Semester 1, 2017
+ * Project Part B - Metro Madness
+ * 
+ * Group 107
+ * Members:
+ * Nate Wangsutthitham
+ * Kolatat Thangkasemvathana
+ * Khai Mei Chin
+ *  
+ */
+
 package com.unimelb.swen30006.metromadness.passengers;
 
 import java.util.ArrayList;
